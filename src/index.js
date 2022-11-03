2ae3cafa460e9dbcaf981253b9ebb81e25c1a8bf
longtk26 <longtk26@gmail.com>
2022-11-04 00:05:35 +0700

    Day-3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
 //HTTP Logger
 app.use(morgan("combined"));
 //Template engine
